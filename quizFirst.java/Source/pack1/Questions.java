e0404a8085303f9f4892e88d8f2f57c7b2d86c5c
ayla-marie <aylawolfgram@gmail.com>
2019-08-21 23:10:24 -0400

    Work on quiz question wording

---
+++ quizFirst.java/Source/pack1/Questions.java
@@ -0,0 +1,90 @@
+package pack1;
+
+public class Questions {
+
+
+	String jp1 = "How do you prefer to work?" +
+	"a. By following a plan/schedule" + "b. By taking tasks day-by-day.";
+	
+	String jp2 = "When you look at your work and personal spaces, they usually are..." +
+	"a. Neat, or at least organized" + "b. Messy. I loose things a lot";
+	
+	String jp3 = "Do you like deadlines?" +
+	"a. Yes. I can pace myself to finish on-time" + "b. No. I get very stressed as the deadline approaches.";
+	
+	String jp4 = "How often do you jump to the wrong conclusion?" +
+	"a. It happens sometimes..." + "b. Rarely. More often, I wait too long to decide.";
+
+	
+	
+	public class Judging {
+		
+		String ft1 = "When things are fair, everyone is treated equally, regardless of circumstance." +
+		"a. True" + "b. False";
+		
+		String ft2 = "When things are fair, personal circumstance is taken into consideration, so that the outcome is more equal." 
+				+ "a. False" + "b. True";
+		
+		String ft3 = "Which is a greater personal strength of yours?" +
+		 "a. Empathy" + "b. Analysis";
+		
+		String ft4 = "Other people ask your opinion because" +
+		"a. You know the facts" + "b. You know how to influence others";
+		
+		public class Feeling {
+			
+			String ie1 = "Do you wear your heart on your sleeve?"
+					+ "a. Yes" + "b. No";
+			
+			String ie2 = "Is it better to stick to your principles or cooperate with your group?"
+					+ "a. cooperate with your group" + "b. stick to your principles";
+			
+			
+			public class Introvert {
+				
+			}
+			public class Extrovert {
+			
+			}
+		}
+	
+		public class Thinking {
+
+			public class Introvert {
+			
+			}
+			public class Extrovert {
+			
+			}
+		}
+	
+	}
+	
+	public class Perceiving {
+		
+		public class Sensing {
+			public class Introvert {
+				
+			}
+			public class Extrovert {
+				
+			}
+		}
+			
+		public class Intuition {
+			public class Introvert {
+				
+			}
+			public class Extrovert {
+				
+			}
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}
+
+
